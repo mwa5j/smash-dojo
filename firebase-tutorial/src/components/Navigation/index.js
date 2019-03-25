279831a26e9323e6d647b3356ecb0d57549ad064
mwa5j <35936584+mwa5j@users.noreply.github.com>
2019-03-25 13:57:09 -0400

    Updated CSS

--- firebase-tutorial/src/components/Navigation/index.js
+++ firebase-tutorial/src/components/Navigation/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Navigation.css'
+
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
@@ -18,23 +20,28 @@ const Navigation = () => (
 )
 
 const NavigationAuth = () => (
-    <ul>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.HOME}>Home</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ACCOUNT}>Account</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
-        <li>
-            <SignOutButton />
-        </li>
-    </ul>
+    <div className="container">
+        <div>
+            <label className="logo"></label>
+            <ul className="listContainer">
+                <li className="li">
+                    <Link className="link" to={ROUTES.LANDING}>Landing</Link>
+                </li>
+                <li className="li">
+                    <Link className="link" to={ROUTES.HOME}>Home</Link>
+                </li>
+                <li className="li">
+                    <Link className="link" to={ROUTES.ACCOUNT}>Account</Link>
+                </li>
+                <li className="li">
+                    <Link className="link" to={ROUTES.ADMIN}>Admin</Link>
+                </li>
+                <li className="li">
+                    <SignOutButton />
+                </li>
+            </ul>
+        </div>
+    </div>
 )
 
 const NavigationNonAuth = () => (
